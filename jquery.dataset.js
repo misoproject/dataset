c9adc73263596ca971b5065d688f637273bda8be
Alex Graul <alex.graul@guardian.co.uk>
2011-09-30 18:41:01 +0100

    modifying column metadata

--- jquery.dataset.js
+++ jquery.dataset.js
@@ -56,6 +56,9 @@ var datatable = $('table#datatable'),
 // what metadata about columns do we want here?
 dataset.column('GER.1.M').metadata
 => {"definition": "Gross Enrolment Ratio for male students, see http://..."}
+dataset.column('GER.1.M').metadata({
+  definition: "Gross Enrolment Ratio for male students, see UNESCO Doc 3.23.332"
+});
 
 // dataset types
 // -------------
