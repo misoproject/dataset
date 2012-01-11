75f8524457df1ab5f3ea28b319661448b3c223d2
Alex Graul <alex.graul@guardian.co.uk>
2012-01-11 15:41:25 +0000

    initial views tests

---
+++ test/unit/views.js
@@ -0,0 +1,23 @@
+module("Dataset Views")
+
+  var ds = new DS({
+    data: { columns : [ 
+      { name : "one",   data : [1, 2, 3] },
+      { name : "two",   data : [4, 5, 6] },
+      { name : "three", data : [7, 8, 9] } 
+    ] },
+    strict: true
+  });
+
+
+  test("Basic View creation", function() {
+    var view = ds.where({});
+    equal(ds.columns[0].data, view.columns[0].data);
+  });
+
+  test("View from row ID array", function() {});
+  test("View from row function", function() {});
+  test("View from column array", function() {});
+  test("", function() {});
+  test("", function() {});
+
