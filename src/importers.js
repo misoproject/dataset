55c49e451cb6603c5e7f43fcfb308be528147c05
Alex Graul <alex.graul@guardian.co.uk>
2012-01-26 10:51:08 +0000

    refactor parter prototype extensions in extend block

--- src/importers.js
+++ src/importers.js
@@ -5,157 +5,160 @@
   // ------ data parsers ---------
   DS.Parsers = function() {};
 
-  /**
-   * Creates an internal representation of a column based on
-   * the form expected by our strict json.
-   * @param {string} name The column name
-   * @param {string} type The type of the data in the column
-   */
-  DS.Parsers.prototype._buildColumn = function(name, type, data) {
-    return {
-      _id : _.uniqueId(),
-      name : name,
-      type : type,
-      data : (data || [])
-    };
-  };
+  _.extend(DS.Parsers.prototype, {
 
-  DS.Parsers.prototype.build = function(options) {
-    var d = {};
+    /**
+    * Creates an internal representation of a column based on
+    * the form expected by our strict json.
+    * @param {string} name The column name
+    * @param {string} type The type of the data in the column
+    */
+    _buildColumn : function(name, type, data) {
+      return {
+        _id : _.uniqueId(),
+        name : name,
+        type : type,
+        data : (data || [])
+      };
+    },
 
-    this._buildColumns(d);
-    this._detectTypes(d);
-    this._cacheColumns(d);
-    this._cacheRows(d);
+    build : function(options) {
+      var d = {};
 
-    return d;
-  }
+      this._buildColumns(d);
+      this._detectTypes(d);
+      this._cacheColumns(d);
+      this._cacheRows(d);
 
-  DS.Parsers.prototype._addValue = function(d, columnName, value) {
-    var colPos = d._columnPositionByName[columnName];
-    d._columns[colPos].data.push(value);
-  };
+      return d;
+    },
 
-  DS.Parsers.prototype._detectTypes = function(d, n) {
+    _addValue : function(d, columnName, value) {
+      var colPos = d._columnPositionByName[columnName];
+      d._columns[colPos].data.push(value);
+    },
 
-    _.each(d._columns, function(column) {
+    _detectTypes : function(d, n) {
 
-      // check if the column already has a type defined. If so, skip
-      // this auth detection phase.
-      if (_.isUndefined(column.type) || column.type === null) {
-        
-        // compute the type by assembling a sample of computed types
-        // and then squashing it to create a unique subset.
-        var type = _.inject(column.data.slice(0, (n || 5)), function(memo, value) {
+      _.each(d._columns, function(column) {
 
-          var t = DS.typeOf(value);
+        // check if the column already has a type defined. If so, skip
+        // this auth detection phase.
+        if (_.isUndefined(column.type) || column.type === null) {
 
-          if (value !== "" && memo.indexOf(t) == -1 && !_.isNull(value)) {
-            memo.push(t);
+          // compute the type by assembling a sample of computed types
+          // and then squashing it to create a unique subset.
+          var type = _.inject(column.data.slice(0, (n || 5)), function(memo, value) {
+
+            var t = DS.typeOf(value);
+
+            if (value !== "" && memo.indexOf(t) == -1 && !_.isNull(value)) {
+              memo.push(t);
+            }
+            return memo;
+          }, []);
+
+          // if we only have one type in our sample, save it as the type
+          if (type.length === 1) {
+            column.type = type[0];
+          } else if (type.length === 0) {
+            // we are assuming that this is a number type because we have
+            // no values in the sample. Unfortuante.
+            column.type = "number";
+          } else {
+            throw new Error("This column seems to have mixed types");
           }
-          return memo;
-        }, []);
-
-        // if we only have one type in our sample, save it as the type
-        if (type.length === 1) {
-          column.type = type[0];
-        } else if (type.length === 0) {
-          // we are assuming that this is a number type because we have
-          // no values in the sample. Unfortuante.
-          column.type = "number";
-        } else {
-          throw new Error("This column seems to have mixed types");
         }
-      }
-      
-    });
-    
-    return d;
-  };
 
-  /**
-  * Used by internal importers to cache the columns and their
-  * positions in a fast hash lookup.
-  * @param d {object} the data object to append cache to.
-  */
-  DS.Parsers.prototype._cacheColumns = function(d) {
-    d._columnPositionByName = {};
-    
-    // cache columns by their column names
-    // TODO: should we cache by _id?
-    _.each(d._columns, function(column, index) {
-      d._columnPositionByName[column.name] = index;
-    });
-
-    return d;
-  };
-
-  /**
-   * Used by internal importers to cache the rows 
-   * in quick lookup tables for any id based operations.
-   * @param d {object} the data object to append cache to.
-   */
-  DS.Parsers.prototype._cacheRows = function(d) {
-    
-    d._rowPositionById = {};
-    d._rowIdByPosition = [];
-  
-    // cache the row id positions in both directions.
-    // iterate over the _id column and grab the row ids
-    _.each(d._columns[d._columnPositionByName._id].data, function(id, index) {
-      d._rowPositionById[id] = index;
-      d._rowIdByPosition.push(id);
-    });  
-
-    // cache the total number of rows. There should be same 
-    // number in each column's data type
-    var rowLengths = _.uniq(
-      _.map(
-        d._columns,
-        function(column) { 
-          return column.data.length;
-        }
-      )
-    );
-
-    if (rowLengths.length > 1) {
-      throw new Error("Row lengths need to be the same. Empty values should be set to null." + _.map(d._columns, function(c) { return c.data + "|||" ; }));
-    } else {
-      d.length = rowLengths[0];
-    }
-
-    return d;
-  };
-
-  /**
-  * Adds an id column to the column definition. If a count
-  * is provided, also generates unique ids.
-  * @param d {object} the data object to modify
-  * @param count {number} the number of ids to generate.
-  */
-  DS.Parsers.prototype._addIdColumn = function(d, count) {
-    // if we have any data, generate actual ids.
-    var ids = [];
-    if (count && count > 0) {
-      _.times(count, function() {
-        ids.push(_.uniqueId());
       });
-    }
-    d._columns.unshift(
-      this._buildColumn("_id", "number", ids)
-    );
-    
-    return d;
-  };
 
-  
-  /**
-   * By default we are assuming that our data is in
-   * the correct form from the fetching.
-   */
-  DS.Parsers.prototype.parse = function(data) {
-    return data;
-  };
+      return d;
+    },
+
+    /**
+    * Used by internal importers to cache the columns and their
+    * positions in a fast hash lookup.
+    * @param d {object} the data object to append cache to.
+    */
+    _cacheColumns : function(d) {
+      d._columnPositionByName = {};
+
+      // cache columns by their column names
+      // TODO: should we cache by _id?
+      _.each(d._columns, function(column, index) {
+        d._columnPositionByName[column.name] = index;
+      });
+
+      return d;
+    },
+
+    /**
+    * Used by internal importers to cache the rows 
+    * in quick lookup tables for any id based operations.
+    * @param d {object} the data object to append cache to.
+    */
+    _cacheRows : function(d) {
+
+      d._rowPositionById = {};
+      d._rowIdByPosition = [];
+
+      // cache the row id positions in both directions.
+      // iterate over the _id column and grab the row ids
+      _.each(d._columns[d._columnPositionByName._id].data, function(id, index) {
+        d._rowPositionById[id] = index;
+        d._rowIdByPosition.push(id);
+      });  
+
+      // cache the total number of rows. There should be same 
+      // number in each column's data type
+      var rowLengths = _.uniq(
+        _.map(
+          d._columns,
+          function(column) { 
+            return column.data.length;
+          }
+          )
+          );
+
+        if (rowLengths.length > 1) {
+          throw new Error("Row lengths need to be the same. Empty values should be set to null." + _.map(d._columns, function(c) { return c.data + "|||" ; }));
+        } else {
+          d.length = rowLengths[0];
+        }
+
+        return d;
+    },
+
+    /**
+    * Adds an id column to the column definition. If a count
+    * is provided, also generates unique ids.
+    * @param d {object} the data object to modify
+    * @param count {number} the number of ids to generate.
+    */
+    _addIdColumn : function(d, count) {
+      // if we have any data, generate actual ids.
+      var ids = [];
+      if (count && count > 0) {
+        _.times(count, function() {
+          ids.push(_.uniqueId());
+        });
+      }
+      d._columns.unshift(
+        this._buildColumn("_id", "number", ids)
+      );
+
+      return d;
+    },
+
+
+    /**
+    * By default we are assuming that our data is in
+    * the correct form from the fetching.
+    */
+    parse : function(data) {
+      return data;
+    }
+  });
 
   // ------ Strict Parser ---------
   /**
