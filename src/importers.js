08daae6ec41bf02d6cb1bb4e00c98667ec730f7f
Alex Graul <alex.graul@guardian.co.uk>
2012-02-29 16:30:13 +0000

    more gnarly xhr stuff to the bottom of importers for clarity

--- src/importers.js
+++ src/importers.js
@@ -1,190 +1,6 @@
 (function(global, _) {
   var DS = (global.DS || (global.DS = {}));
 
-  // this XHR code is from @rwldron.
-  var _xhrSetup = {
-    url       : "",
-    data      : "",
-    dataType  : "",
-    success   : function() {},
-    type      : "GET",
-    async     : true,
-    xhr : function() {
-      return new global.XMLHttpRequest();
-    }
-  }, rparams = /\?/;
-
-  DS.Xhr = function(options) {
-
-    // json|jsonp etc.
-    options.dataType = options.dataType && options.dataType.toLowerCase() || null;
-
-    if (options.dataType && 
-      (options.dataType === "jsonp" || options.dataType === "script" )) {
-
-        DS.Xhr.getJSONP(
-          options.url,
-          options.success,
-          options.dataType === "script",
-          options.error
-        );
-
-        return;
-      }
-
-      var settings = _.extend({}, _xhrSetup, options);
-
-      // create new xhr object
-      settings.ajax = settings.xhr();
-
-      if (settings.ajax) {
-        if (settings.type === "GET" && settings.data) {
-
-          //  append query string
-          settings.url += (rparams.test(settings.url) ? "&" : "?") + settings.data;
-
-          //  Garbage collect and reset settings.data
-          settings.data = null;
-        }
-
-        settings.ajax.open(settings.type, settings.url, settings.async);
-        settings.ajax.send(settings.data || null);
-
-        return DS.Xhr.httpData(settings);
-      }
-  };
-
-  DS.Xhr.getJSONP = function(url, success, isScript, error) {
-    // If this is a script request, ensure that we do not 
-    // call something that has already been loaded
-    if (isScript) {
-
-      var scripts = document.querySelectorAll("script[src=\"" + url + "\"]");
-
-      //  If there are scripts with this url loaded, early return
-      if (scripts.length) {
-
-        //  Execute success callback and pass "exists" flag
-        if (success) { 
-          success(true);
-        }
-
-        return;
-      }
-    } 
-
-    var head    = document.head || 
-    document.getElementsByTagName("head")[0] || 
-    document.documentElement,
-
-    script    = document.createElement("script"),
-    paramStr  = url.split("?")[ 1 ],
-    isFired   = false,
-    params    = [],
-    callback, parts, callparam;
-
-    // Extract params
-    if (paramStr && !isScript) {
-      params = paramStr.split("&");
-    }
-    if (params.length) {
-      parts = params[params.length - 1].split("=");
-    }
-    callback = params.length ? (parts[ 1 ] ? parts[ 1 ] : parts[ 0 ]) : "jsonp";
-
-    if (!paramStr && !isScript) {
-      url += "?callback=" + callback;
-    }
-
-    if (callback && !isScript) {
-
-      // If a callback name already exists
-      if (!!window[callback]) {
-        callback = callback + (+new Date()) + _.uniqueId();
-      }
-
-      //  Define the JSONP success callback globally
-      window[callback] = function(data) {
-        if (success) { 
-          success(data);
-        }
-        isFired = true;
-      };
-
-      //  Replace callback param and callback name
-      url = url.replace(parts.join("="), parts[0] + "=" + callback);
-    }
-
-    script.onload = script.onreadystatechange = function() {
-      if (!script.readyState || /loaded|complete/.test(script.readyState)) {
-
-        //  Handling remote script loading callbacks
-        if (isScript) {
-
-          //  getScript
-          if (success) { 
-            success();
-          }
-        }
-
-        //  Executing for JSONP requests
-        if (isFired) {
-
-          //  Garbage collect the callback
-          delete window[callback];
-
-          //  Garbage collect the script resource
-          head.removeChild(script);
-        }
-      }
-    };
-
-    script.onerror = function(e) {
-      if (error) { 
-        error.call(null);
-      }
-    };
-
-    script.src = url;
-    head.insertBefore(script, head.firstChild);
-    return;
-  };
-
-  DS.Xhr.httpData = function(settings) {
-    var data, json = null;
-
-    settings.ajax.onreadystatechange = function() {
-      if (settings.ajax.readyState === 4) {
-        try {
-          json = JSON.parse(settings.ajax.responseText);
-        } catch (e) {
-          // suppress
-        }
-
-        data = {
-          xml : settings.ajax.responseXML,
-          text : settings.ajax.responseText,
-          json : json
-        };
-
-        if (settings.dataType) {
-          data = data[settings.dataType];
-        }
-
-        // if we got an ok response, call success, otherwise fail.
-        if (/(2..)/.test(settings.ajax.status)) {
-          settings.success.call(settings.ajax, data);  
-        } else {
-          if (settings.error) {
-            settings.error.call(null, settings.ajax.statusText);
-          }
-        }
-      }
-    };
-
-    return data;
-  };
-
   DS.Importers = function(data, options) {};
 
   /**
@@ -198,7 +14,7 @@
   };
 
   /**
-  * Local data importer is responsible for just using 
+  * Local data importer is responsible for just using
   * a data object and passing it appropriatly.
   */
   DS.Importers.Local = function(options) {
@@ -223,7 +39,7 @@
         options));
 
         var parsedData = this.parser.build();
-        options.success(parsedData);     
+        options.success(parsedData);
     }
   });
 
@@ -262,18 +78,203 @@
         this.parser = new this.parser(data, options);
 
         var parsedData = this.parser.build();
-        options.success(parsedData);  
+        options.success(parsedData);
 
       }, this);
 
       // make ajax call to fetch remote url.
-      DS.Xhr(_.extend(this.params, { 
+      DS.Xhr(_.extend(this.params, {
         success : callback,
         error   : options.error
       }));
     }
   }
+
 );
+
+// this XHR code is from @rwldron.
+var _xhrSetup = {
+  url       : "",
+  data      : "",
+  dataType  : "",
+  success   : function() {},
+  type      : "GET",
+  async     : true,
+  xhr : function() {
+    return new global.XMLHttpRequest();
+  }
+}, rparams = /\?/;
+
+DS.Xhr = function(options) {
+
+  // json|jsonp etc.
+  options.dataType = options.dataType && options.dataType.toLowerCase() || null;
+
+  if (options.dataType &&
+    (options.dataType === "jsonp" || options.dataType === "script" )) {
+
+      DS.Xhr.getJSONP(
+        options.url,
+        options.success,
+        options.dataType === "script",
+        options.error
+      );
+
+      return;
+    }
+
+    var settings = _.extend({}, _xhrSetup, options);
+
+    // create new xhr object
+    settings.ajax = settings.xhr();
+
+    if (settings.ajax) {
+      if (settings.type === "GET" && settings.data) {
+
+        //  append query string
+        settings.url += (rparams.test(settings.url) ? "&" : "?") + settings.data;
+
+        //  Garbage collect and reset settings.data
+        settings.data = null;
+      }
+
+      settings.ajax.open(settings.type, settings.url, settings.async);
+      settings.ajax.send(settings.data || null);
+
+      return DS.Xhr.httpData(settings);
+    }
+};
+
+DS.Xhr.getJSONP = function(url, success, isScript, error) {
+  // If this is a script request, ensure that we do not
+  // call something that has already been loaded
+  if (isScript) {
+
+    var scripts = document.querySelectorAll("script[src=\"" + url + "\"]");
+
+    //  If there are scripts with this url loaded, early return
+    if (scripts.length) {
+
+      //  Execute success callback and pass "exists" flag
+      if (success) {
+        success(true);
+      }
+
+      return;
+    }
+  }
+
+  var head    = document.head ||
+  document.getElementsByTagName("head")[0] ||
+  document.documentElement,
+
+  script    = document.createElement("script"),
+  paramStr  = url.split("?")[ 1 ],
+  isFired   = false,
+  params    = [],
+  callback, parts, callparam;
+
+  // Extract params
+  if (paramStr && !isScript) {
+    params = paramStr.split("&");
+  }
+  if (params.length) {
+    parts = params[params.length - 1].split("=");
+  }
+  callback = params.length ? (parts[ 1 ] ? parts[ 1 ] : parts[ 0 ]) : "jsonp";
+
+  if (!paramStr && !isScript) {
+    url += "?callback=" + callback;
+  }
+
+  if (callback && !isScript) {
+
+    // If a callback name already exists
+    if (!!window[callback]) {
+      callback = callback + (+new Date()) + _.uniqueId();
+    }
+
+    //  Define the JSONP success callback globally
+    window[callback] = function(data) {
+      if (success) {
+        success(data);
+      }
+      isFired = true;
+    };
+
+    //  Replace callback param and callback name
+    url = url.replace(parts.join("="), parts[0] + "=" + callback);
+  }
+
+  script.onload = script.onreadystatechange = function() {
+    if (!script.readyState || /loaded|complete/.test(script.readyState)) {
+
+      //  Handling remote script loading callbacks
+      if (isScript) {
+
+        //  getScript
+        if (success) {
+          success();
+        }
+      }
+
+      //  Executing for JSONP requests
+      if (isFired) {
+
+        //  Garbage collect the callback
+        delete window[callback];
+
+        //  Garbage collect the script resource
+        head.removeChild(script);
+      }
+    }
+  };
+
+  script.onerror = function(e) {
+    if (error) {
+      error.call(null);
+    }
+  };
+
+  script.src = url;
+  head.insertBefore(script, head.firstChild);
+  return;
+};
+
+DS.Xhr.httpData = function(settings) {
+  var data, json = null;
+
+  settings.ajax.onreadystatechange = function() {
+    if (settings.ajax.readyState === 4) {
+      try {
+        json = JSON.parse(settings.ajax.responseText);
+      } catch (e) {
+        // suppress
+      }
+
+      data = {
+        xml : settings.ajax.responseXML,
+        text : settings.ajax.responseText,
+        json : json
+      };
+
+      if (settings.dataType) {
+        data = data[settings.dataType];
+      }
+
+      // if we got an ok response, call success, otherwise fail.
+      if (/(2..)/.test(settings.ajax.status)) {
+        settings.success.call(settings.ajax, data);
+      } else {
+        if (settings.error) {
+          settings.error.call(null, settings.ajax.statusText);
+        }
+      }
+    }
+  };
+
+  return data;
+};
 
 
 
