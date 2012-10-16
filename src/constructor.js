645a88fd89d7b8e8c73fdc71df4f8a1b9016e311
Rich Harris <richard.a.harris@gmail.com>
2012-10-16 09:54:59 +0100

    tidy up, bug fix

--- src/constructor.js
+++ src/constructor.js
@@ -1,10 +1,10 @@
-(function(global, _, moment) {
+(function(global) {
 
 /**
   * Instantiates a new dataset.
   * Parameters:
-  * options - optional parameters. 
-  *   data : "Object - an actual javascript object that already contains the data",  
+  * options - optional parameters.
+  *   data : "Object - an actual javascript object that already contains the data",
   *   url : "String - url to fetch data from",
   *   sync : Set to true to be able to bind to dataset changes. False by default.
   *   jsonp : "boolean - true if this is a jsonp request",
@@ -13,17 +13,17 @@
   *   extract : "function to apply to JSON before internal interpretation, optional"
   *   ready : the callback function to act on once the data is fetched. Isn't reuired for local imports
   *           but is required for remote url fetching.
-  *   columns: A way to manually override column type detection. Expects an array of 
-  *            objects of the following structure: 
-  *           { name : 'columnname', type: 'columntype', 
+  *   columns: A way to manually override column type detection. Expects an array of
+  *            objects of the following structure:
+  *           { name : 'columnname', type: 'columntype',
   *             ... (additional params required for type here.) }
   *   comparator : function (optional) - takes two rows and returns 1, 0, or -1  if row1 is
-  *     before, equal or after row2. 
+  *     before, equal or after row2.
   *   deferred : by default we use underscore.deferred, but if you want to pass your own (like jquery's) just
   *              pass it here.
-  *   importer : The classname of any importer (passes through auto detection based on parameters. 
+  *   importer : The classname of any importer (passes through auto detection based on parameters.
   *              For example: <code>Miso.Importers.Polling</code>.
-  *   parser   : The classname of any parser (passes through auto detection based on parameters. 
+  *   parser   : The classname of any parser (passes through auto detection based on parameters.
   *              For example: <code>Miso.Parsers.Delimited</code>.
   *   resetOnFetch : set to true if any subsequent fetches after first one should overwrite the
   *                  current data.
@@ -31,6 +31,7 @@
   *   interval : Polling interval. Set to any value in milliseconds to enable polling on a url.
   }
   */
+  global.Miso = global.Miso || {};
   global.Miso.Dataset = function(options) {
     this.length = 0;
     
@@ -43,4 +44,4 @@
       this._initialize(options);
     }
   };
-}(this, _, moment));
+}(this));
